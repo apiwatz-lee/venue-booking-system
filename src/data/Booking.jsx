950f67595a782dfebba1567bfa7e26d5ced43615
Apiwat Lee <apiwatz.lee@gmail.com>
2024-09-16 15:24:52 +0700

    edit:update bookings

--- src/data/Booking.jsx
+++ src/data/Booking.jsx
@@ -2,15 +2,15 @@ export const bookings = [
   {
     id: 1,
     roomId: "A101",
-    startTime: "2024-09-16 15:00:00",
-    endTime: "2024-09-16 16:00:00",
+    startTime: "2024-09-17 11:00:00",
+    endTime: "2024-09-17 12:00:00",
     title: "Lunch with Petr",
   },
   {
     id: 2,
     roomId: "A101",
-    startTime: "2024-09-16 16:00:00",
-    endTime: "2024-09-16 17:00:00",
+    startTime: "2024-09-17 13:00:00",
+    endTime: "2024-09-17 15:00:00",
     title: "Sales Weekly Meeting",
   },
   {
